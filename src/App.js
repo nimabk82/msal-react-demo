c1066e542333bac4f5b1f5a3e80c1a1d4fe77f55
nimaboakei@sehc.com <nimaboakei@sehc.com>
2023-12-30 17:41:19 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import Grid from "@mui/material/Grid";
-import { PageLayout } from "./components/PageLayout";
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-
+import { PageLayout } from "./components/PageLayout";
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
+import { MsalProvider, useIsAuthenticated, useMsal } from "@azure/msal-react";
+import { InteractionRequiredAuthError } from "@azure/msal-browser";
+import Grid from "@mui/material/Grid";
 
-function App() {
+function App({ msalInstance }) {
     return (
-        <PageLayout>
-            <Grid container justifyContent="center">
-                <Pages />
-            </Grid>
-        </PageLayout>
+        <MsalProvider instance={msalInstance}>
+            <PageLayout>
+                <Grid container justifyContent="center">
+                    <Pages />
+                </Grid>
+            </PageLayout>
+        </MsalProvider>
     );
 }
 
 const Pages = () => {
+    const { instance } = useMsal();
+    const isAuthenticated = useIsAuthenticated();
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            instance.ssoSilent({
+                scopes: ["user.read"],
+                // loginHint: "", 
+            }).then((response) => {
+                instance.setActiveAccount(response.account);
+            }).catch(error => {
+                    if (error instanceof InteractionRequiredAuthError) {
+                        instance.loginRedirect();
+                    }
+                });
+        }
+    // eslint-disable-next-line
+    }, []);
+
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
         </Routes>
-    );
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./styles/theme";
 
 import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { PublicClientApplication, EventType } from "@azure/msal-browser";
+
+const pca = new PublicClientApplication({
+  auth: {
+    clientId: "d5a97498-a9f0-4007-9f4a-d16592145e79",
+    authority:
+      "https://login.microsoftonline.com/bf475492-067f-4d6e-989f-776b97a19cd9",
+    redirectUri: "/",
+    postLogoutRedirectUri: "/",
+    clientCapabilities: ["CP1"],
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false,
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPII) => {
+        console.log(message);
+      },
+      logLevel: "Verbose",
+    },
+  },
+});
+
+pca.addEventCallback((event) => {
+  if (event.eventType === EventType.LOGIN_SUCCESS) {
+    pca.setActiveAccount(event.payload.account);
+  }
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <ThemeProvider theme={theme}>
-                <App />
-            </ThemeProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App msalInstance={pca} />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
