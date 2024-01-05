6aed3d12608db41ec5aa243af2f7ff10d36097f8
nima bk <>
2024-01-05 13:47:46 -0500

    .

--- src/components/DeeplinkButton.jsx
+++ src/components/DeeplinkButton.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 export const DeeplinkButton = () => {
 
 
+  function launchAppOnAndroidDevice() {
+    const url = "https://purple-water-0475ec410.4.azurestaticapps.net/test/nima#Intent;scheme=https;package=com.msalrn;end";
+    window.location.replace(url);
+ }
+
   return (
     <div style={{marginTop:40}}>
     <div>
@@ -10,6 +15,9 @@ export const DeeplinkButton = () => {
     </div>
        <div>
     <Link to="https://purple-water-0475ec410.4.azurestaticapps.net/test/nima" >https://purple-water-0475ec410.4.azurestaticapps.net/test/nima</Link></div>
+       <div>
+       <button onclick={launchAppOnAndroidDevice}>Open App</button>
+        </div>
     </div>
-  );
+  )
 };
