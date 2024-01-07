5f1407248b1be1d55daf982416ed1ee04d15a02d
nima bk <>
2024-01-07 11:55:24 -0500

    .

--- src/components/DeeplinkButton.jsx
+++ src/components/DeeplinkButton.jsx
@@ -1,14 +1,14 @@
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 
 const URL1 = 'https://purple-water-0475ec410.4.azurestaticapps.net'
 const URL2 = 'https://msal-react-demo.vercel.app'
 export const DeeplinkButton = () => {
 
 
-  function launchAppOnAndroidDevice(e , url,newtab) {
-    e.preventDefault();
-    window.location.replace(url);
- }
+//   function launchAppOnAndroidDevice(e , url,newtab) {
+//     e.preventDefault();
+//     window.location.replace(url);
+//  }
 
 
  function launchAppOnAndroidDevice1(e , url,newtab) {
@@ -22,8 +22,12 @@ export const DeeplinkButton = () => {
       <td onClick={()=> window.open(`${URL2}/test/nima111`, "_blank")}>text1</td>
       <td onClick={(e)=> launchAppOnAndroidDevice1(e,`${URL2}/test/nima111`)}>text2</td>
 
+
+      <td onClick={()=> window.open(`${URL1}/test/nima111`, "_blank")}>text1</td>
+      <td onClick={(e)=> launchAppOnAndroidDevice1(e,`${URL1}/test/nima111`)}>text2</td>
+
        <div>
-       <Button color="inherit" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}/test/nima111`)} >
+       {/* <Button color="inherit" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}/test/nima111`)} >
      Deep link to 1
     </Button>
 
@@ -33,14 +37,14 @@ export const DeeplinkButton = () => {
 
     <Button color="inherit" onClick={(e) => launchAppOnAndroidDevice(e , `${URL2}/test/nima111`)}>
     Deep link to 3
-    </Button>
+    </Button> */}
 
-    <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}/test/nima111`)}>Deep link to 4</a>
+    {/* <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}/test/nima111`)}>Deep link to 4</a>
     <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL2}/test/nima111`)}>Deep link to 5</a>
-    <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL2}/test/nima111`)} target="_blank">Deep link to 6</a>
+    <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL2}/test/nima111`)} target="_blank">Deep link to 6</a> */}
 
 
-    <a href={`${URL1}/test/nima111`} target="_blank" >aaaaaaaa</a>
+    {/* <a href={`${URL1}/test/nima111`} target="_blank" >aaaaaaaa</a> */}
         </div>
     </div>
   )
