956a1209771076fe940dcfc1f8258a3a4af6e6d8
nima bk <>
2024-01-07 11:48:42 -0500

    .

--- src/components/DeeplinkButton.jsx
+++ src/components/DeeplinkButton.jsx
@@ -19,7 +19,7 @@ export const DeeplinkButton = () => {
 
   return (
     <div style={{marginTop:40}}>
-      <td onClick={()=> window.open(`${URL1}/test/nima111`, "_blank")}>text1</td>
+      <td onClick={()=> window.open(`${URL2}/test/nima111`, "_blank")}>text1</td>
       <td onClick={(e)=> launchAppOnAndroidDevice1(e,`${URL2}/test/nima111`)}>text2</td>
 
        <div>
