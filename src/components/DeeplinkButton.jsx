e61acccb0d047a753ce6efd2707c38a5ff7067ca
nima bk <>
2024-01-07 11:48:08 -0500

    .

--- src/components/DeeplinkButton.jsx
+++ src/components/DeeplinkButton.jsx
@@ -5,21 +5,42 @@ const URL2 = 'https://msal-react-demo.vercel.app'
 export const DeeplinkButton = () => {
 
 
-  function launchAppOnAndroidDevice(url) {
+  function launchAppOnAndroidDevice(e , url,newtab) {
+    e.preventDefault();
     window.location.replace(url);
  }
 
+
+ function launchAppOnAndroidDevice1(e , url,newtab) {
+  e.preventDefault();
+  window.open(url)
+}
+
+
   return (
     <div style={{marginTop:40}}>
+      <td onClick={()=> window.open(`${URL1}/test/nima111`, "_blank")}>text1</td>
+      <td onClick={(e)=> launchAppOnAndroidDevice1(e,`${URL2}/test/nima111`)}>text2</td>
+
        <div>
-       <Button color="inherit" onClick={() => launchAppOnAndroidDevice(`${URL1}/test/nima111`)}>
-     Deep link to "https://purple-water-0475ec410.4.azurestaticapps.net/test/nim
+       <Button color="inherit" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}/test/nima111`)} >
+     Deep link to 1
     </Button>
 
-    <Button color="inherit" onClick={() => launchAppOnAndroidDevice(`${URL2}/test/nima111`)}>
-     Deep link to "hhttps://msal-react-demo.vercel.app/test/nima111"
+    <Button color="inherit" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}/test/nima111`)} target="_blank">
+    Deep link to 2
     </Button>
-    <a href={`${URL1}/test/nima111`} >aaaaaaaa</a>
+
+    <Button color="inherit" onClick={(e) => launchAppOnAndroidDevice(e , `${URL2}/test/nima111`)}>
+    Deep link to 3
+    </Button>
+
+    <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}/test/nima111`)}>Deep link to 4</a>
+    <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL2}/test/nima111`)}>Deep link to 5</a>
+    <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL2}/test/nima111`)} target="_blank">Deep link to 6</a>
+
+
+    <a href={`${URL1}/test/nima111`} target="_blank" >aaaaaaaa</a>
         </div>
     </div>
   )
