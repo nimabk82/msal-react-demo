1fcd6d6dcad4651fefedb05ab24f9afcccc56dca
nima bk <>
2024-01-07 12:32:16 -0500

    .

--- src/components/DeeplinkButton.jsx
+++ src/components/DeeplinkButton.jsx
@@ -20,40 +20,6 @@ export const DeeplinkButton = () => {
     <Button color="inherit" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}`)} >
      Deep link to app
     </Button>
-
-    <Button color="inherit" onClick={() => window.location.assign(`${URL1}/test/nima`)} >
-     Deep link to /test/nima
-    </Button>
-      {/* <td onClick={()=> window.open(`${URL2}/test/nima111`, "_blank")}>text1</td>
-      <td onClick={(e)=> launchAppOnAndroidDevice1(e,`${URL2}/test/nima111`)}>text2</td>
-      <td onClick={(e)=> launchAppOnAndroidDevice1(e,`${URL2}`)}>text3</td>
-      <td onClick={()=> window.open(`${URL2}/test/nima111`)}>text4</td>
-      <td onClick={()=>  window.location.replace(`${URL2}/test/nima111`)}>text5</td>
-
-
-      <td onClick={()=> window.open(`${URL1}/test/nima111`, "_blank")}>text1</td>
-      <td onClick={(e)=> launchAppOnAndroidDevice1(e,`${URL1}/test/nima111`)}>text2</td>
-
-       <div> */}
-       {/* <Button color="inherit" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}/test/nima111`)} >
-     Deep link to 1
-    </Button>
-
-    <Button color="inherit" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}/test/nima111`)} target="_blank">
-    Deep link to 2
-    </Button>
-
-    <Button color="inherit" onClick={(e) => launchAppOnAndroidDevice(e , `${URL2}/test/nima111`)}>
-    Deep link to 3
-    </Button> */}
-
-    {/* <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL1}/test/nima111`)}>Deep link to 4</a>
-    <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL2}/test/nima111`)}>Deep link to 5</a>
-    <a href="javascript: false" onClick={(e) => launchAppOnAndroidDevice(e , `${URL2}/test/nima111`)} target="_blank">Deep link to 6</a> */}
-
-
-    {/* <a href={`${URL1}/test/nima111`} target="_blank" >aaaaaaaa</a> */}
-        {/* </div> */}
     </div>
   )
 };
