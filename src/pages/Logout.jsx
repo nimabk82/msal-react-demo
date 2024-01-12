da5c91f39618ca081e9a7be530d86e96dee3470e
nima bk <>
2024-01-12 12:34:59 -0500

    .

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,15 +1,28 @@
 import React, { useEffect } from "react";
 import { useMsal } from "@azure/msal-react";
-import { BrowserUtils } from "@azure/msal-browser";
+// import { BrowserUtils } from "@azure/msal-browser";
 
 export function Logout() {
     const { instance } = useMsal();
 
+   
+
     useEffect(() => {
-        instance.logoutRedirect({
-            account: instance.getActiveAccount(),
-            onRedirectNavigate: () => !BrowserUtils.isInIframe()
-        })
+        // instance.logoutRedirect({
+        //     account: instance.getActiveAccount(),
+        //     onRedirectNavigate: () => !BrowserUtils.isInIframe()
+        // })
+
+        const handleSignOut = async () => {
+            const logoutRequest = {
+              account: instance.getActiveAccount(),
+        postLogoutRedirectUri: "https://purple-water-0475ec410.4.azurestaticapps.net/logout"}
+        console.log(logoutRequest);
+        
+            instance
+              .logoutRedirect(logoutRequest)
+          };
+         handleSignOut()
     }, [ instance ]);
 
     return (
